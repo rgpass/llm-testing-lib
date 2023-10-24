f8a4eedcad653246f69ee505b1cb3bb64bddfcbf
Gerry Pass <rgpass@gmail.com>
2023-10-24 10:21:59 -0400

    Add a basic boolean result

--- binary-result.test.ts
+++
@@ -1,9 +0,0 @@
-describe("Binary results", () => {
-  describe("when the statement is true", () => {
-    it("returns 1", () => {
-      const response = "1";
-
-      expect(Number(response)).toBe(1);
-    });
-  });
-});
